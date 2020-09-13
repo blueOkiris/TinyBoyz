d2c0eb4d140e6dde0aa5cd115d2e349fe4a35736
Dylan Turner <dylantdmt@gmail.com>
2020-09-13 04:02:49 -0400

    Implemented slave part of i2c library

--- TinyProgSD/TinyProgSD.ino
+++ TinyProgSD/TinyProgSD.ino
@@ -1,5 +1,18 @@
+#include <TinyWire.hpp>
+
+#define SDA_PIN 4
+#define SCL_PIN 3
+#define ADDR    0x01
+
+TinyWire::SlaveInterface i2c(SDA_PIN, SCL_PIN);
+char dataBuff[8];
+
 void setup() {
-    
+    dataBuff[0] = 0x00;
+    while(dataBuff[0] != '?') {
+        i2c.read(ADDR);
+    }
+    i2c.write(ADDR, 'R');
 }
 
 void loop() {
--- shared/TinyWire/TinyWire.cpp
+++ shared/TinyWire/TinyWire.cpp
@@ -3,6 +3,79 @@
 
 using namespace TinyWire;
 
+Interface::Interface(char sdaPin, char sclPin) {
+    _readByte = 0;
+    _sdaMask = 0x01 << sdaPin;
+    _sclMask = 0x01 << sclPin;
+}
+
+MasterInterface::MasterInterface(char sdaPin, char sclPin) :
+        Interface(sdaPin, sclPin) {
+    DDRB |= _sclMask;
+    PORTB |= _sclMask;
+}
+
+SlaveInterface::SlaveInterface(char sdaPin, char sclPin) :
+        Interface(sdaPin, sclPin) {
+    DDRB &= ~_sclMask;
+}
+
+char MasterInterface::read(char addr) {
+    _readByte = 0;
+    
+    _writeAddr(addr, 1);
+    DDRB &= ~_sdaMask;
+    
+    for(i = 7; i >= 0; i--) {
+        PORTB &= ~_sclMask;
+        _readByte |= (PINB & _sdaMask != 0) << i;
+        PORTB |= _sclMask;
+    }
+    
+    _waitForAcknowledge();
+    _stop();
+    
+    return _readByte;
+}
+
+void MasterInterface::write(char addr, char data) {
+    _writeAddr(addr, 0);
+    DDRB |= _sdaMask;
+    
+    for(i = 7; i >= 0; i--) {
+        PORTB = ((data >> i) & 0x01) ?
+            (PORTB | _sdaMask) :
+            (PORTB & _sdaMask);
+        _pulseClock();
+    }
+    
+    _waitForAcknowledge();
+    _stop();
+}
+
+char SlaveInterface::read(char addr) {
+    _waitUntilMasterTalking(addr);
+    _sendAcknowledge();
+    _retrieveData();
+    
+    return _readByte;
+}
+
+void SlaveInterface::write(char addr, char data) {
+    _waitUntilMasterTalking(addr);
+    _sendAcknowledge();
+    
+    DDRB |= _sdaMask;
+    
+    for(i = 7; i >= 0; i--) {
+        while(PORTB & _sclMask != 0);
+        PORTB = ((data >> i) & 0x01) ?
+            (PORTB | _sdaMask) :
+            (PORTB & _sdaMask);
+        while(PORTB & _sclMask == 0);
+    }
+}
+
 void MasterInterface::_pulseClock() {
     PORTB |= _sclMask;
     PORTB &= ~_sclMask;
@@ -42,44 +115,51 @@ void MasterInterface::_stop() {
     PORTB |= _sclMask;
 }
 
-MasterInterface::MasterInterface(char sdaPin, char sclPin) {
-    _readByte = 0;
-    _sdaMask = 0x01 << sdaPin;
-    _sclMask = 0x01 << sclPin;
-    
-    DDRB |= _sclMask;
-    PORTB |= _sclMask;
-}
-
-char MasterInterface::read(char addr) {
-    _readByte = 0;
-    
-    _writeAddr(addr, 1);
+void SlaveInterface::_waitForMaster() {
+    // Wait for start signal
     DDRB &= ~_sdaMask;
-    
-    for(i = 7; i >= 0; i--) {
-        PORTB &= ~_sclMask;
-        _readByte |= (PINB & _sdaMask != 0) << i;
-        PORTB |= _sclMask;
+    while(1) {
+        if(PINB & _sclMask != 0 && PINB & _sdaMask == 0) {
+            while(PINB & _sclMask != 0) {
+                if(PINB & _sdaMask != 0) {
+                    break;
+                }
+            }
+            
+            if(PINB & _sdaMask == 0) {
+                break;
+            }
+        }
     }
-    
-    _waitForAcknowledge();
-    _stop();
-    
-    return _readByte;
 }
 
-void MasterInterface::write(char addr, char data) {
-    _writeAddr(addr, 0);
-    DDRB |= _sdaMask;
+void SlaveInterface::_retrieveData() {
+    DDRB &= ~_sdaMask;
+    _readByte = 0;
     
     for(i = 7; i >= 0; i--) {
-        PORTB = ((data >> i) & 0x01) ?
-            (PORTB | _sdaMask) :
-            (PORTB & _sdaMask);
-        _pulseClock();
+        while(PORTB & _sclMask != 0);
+        _readByte |= (PINB & _sdaMask != 0) << i;
+        while(PORTB & _sclMask == 0);
     }
-    
-    _waitForAcknowledge();
-    _stop();
+}
+
+void SlaveInterface::_sendAcknowledge() {
+    while(PORTB & _sclMask != 0);
+    DDRB |= _sdaMask;
+    PORTB &= ~_sdaMask;
+    DDRB &= ~_sdaMask;
+    while(PORTB & _sclMask == 0);
+}
+
+// Check that the master is talking to self
+void SlaveInterface::_waitUntilMasterTalking(char addr) {
+    while(1) {
+        _waitForMaster();
+        _retrieveData(); // Will be address
+        
+        if(_readByte == addr) {
+            break;
+        }
+    }
 }
